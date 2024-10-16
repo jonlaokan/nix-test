71b7fdc9aac9d3664778f09c7423aeafccbaf080
Jonathan Lao-Kan <jonathan.lao-kan@stack-labs.com>
2024-10-17 01:49:56 +0200

    shell.nix

---
+++ shell.nix
@@ -0,0 +1,19 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+pkgs.mkShell {
+    packages = [
+      pkgs.nodejs
+      pkgs.python3
+      pkgs.lolcat
+      pkgs.cowsay
+    ];
+
+    shellHook = ''
+      cowsay "Welcome into you dev shell" | lolcat
+    '';
+
+    FOO = "BAR";
+    BAR = "FOO";
+
+    LD_LIBRARY_PATH = "${pkgs.lib.makeLibraryPath [pkgs.ncurses]}";
+}
